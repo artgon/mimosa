4664ed44dae7ef49533d169087c947d692c88585
Arthur Gonigberg <artgon@gmail.com>
2013-11-12 21:45:07 -0800

    quit works

--- src/irc_test.erl
+++ src/irc_test.erl
@@ -4,22 +4,32 @@
 
 -export([init/1, code_change/3, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
 
--export([start/0]).
+-export([start/0, stop/1]).
 
+-record(state, {socket}).
 
 start() ->
   lager:info("Starting IRC test"),
-  State = {},
-	gen_server:start_link({local, ?MODULE}, ?MODULE, State, []).
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
-init(State) ->
+stop(Pid) -> 
+  lager:info("Stopping IRC test"),
+	gen_server:call(Pid, stop).
+
+
+init([]) ->
   lager:info("Init IRC test"),
   {ok, Socket} = gen_tcp:connect("irc.freenode.net", 6667, [binary, {active,true}]),
   gen_tcp:send(Socket, "NICK artgon_1\r\n"),
   gen_tcp:send(Socket, "USER artgon_1 8 * : Arty G\r\n"),
-  {ok, State}.
+  {ok, #state{socket=Socket}}.
 
 code_change(_OldVersion, State, _Extra) -> {ok, State}.
+
+handle_call(stop, _Caller, State) -> 
+  lager:info("Call IRC test"),
+  gen_tcp:send(State#state.socket, "QUIT :ADIOS\r\n"),
+  {stop, normal, ok, State};
 
 handle_call(Req, _Caller, State) -> 
   lager:info("Call IRC test"),
@@ -38,7 +48,7 @@ handle_info(Req, State) ->
   lager:info("Unkown message: ~w~n", [Req]),
   {noreply, State}.
 
-terminate(_Reason, _State) -> 
+terminate(Reason, State) -> 
   lager:info("Terminate IRC test"),
   ok.
 
